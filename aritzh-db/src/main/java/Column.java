9d53c149fdaf137ec82e3cba9fac7f70e5cbf8e9
Aritz Lopez <aritzhack@gmail.com>
2015-12-24 14:23:51 +0100

    + Added more methods

--- aritzh-db/src/main/java/Column.java
+++ aritzh-db/src/main/java/Column.java
@@ -7,12 +7,23 @@ public class Column {
 	private ColumnType type;
 	private String[] formatArgs;
 
+	private boolean isUnique = false;
+	private boolean isIndex = false;
+
 	public Column(String name, String format) {
 		this.name = name;
 		String[] tokens = format.split(":");
 		this.type = ColumnType.parse(tokens[0]);
 		this.formatArgs = new String[tokens.length - 1];
 		System.arraycopy(tokens, 1, this.formatArgs, 0, tokens.length - 1);
+
+		for(String a : this.formatArgs) {
+			if("unique".equalsIgnoreCase(a.trim())) {
+				this.isUnique = true;
+			} else if("index".equalsIgnoreCase(a.trim())) {
+				this.isIndex = true;
+			}
+		}
 	}
 
 	public String getName() {
@@ -25,5 +36,13 @@ public class Column {
 
 	public String[] getFormatArgs() {
 		return formatArgs;
+	}
+
+	public boolean isUnique() {
+		return isUnique;
+	}
+
+	public boolean isIndex() {
+		return isIndex;
 	}
 }
--- aritzh-db/src/main/java/Table.java
+++ aritzh-db/src/main/java/Table.java
@@ -1,5 +1,8 @@
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import io.github.aritzhack.aritzh.util.Nullable;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -10,11 +13,34 @@ public class Table {
 	private final Column[] columns;
 	private final Set<String[]> entries = Sets.newHashSet();
 
+	private final Map<String, Map<String, String[]>> indices = Maps.newHashMap();
+	private final Set<Integer> indexColumns = Sets.newHashSet();
+
 	public Table(Column[] columns) {
 		this.columns = columns;
+		for (int i = 0; i < this.columns.length; i++) {
+			if (this.columns[i].isIndex()) {
+				indexColumns.add(i);
+				indices.put(this.columns[i].getName().toLowerCase().trim(), Maps.<String, String[]>newHashMap());
+			}
+		}
 	}
 
 	public void addEntry(String... entry) {
 		this.entries.add(entry);
+
+		for (Integer i : this.indexColumns) {
+			indices.get(this.columns[i].getName().toLowerCase().trim()).put(entry[i].toLowerCase().trim(), entry);
+		}
+	}
+
+	@Nullable
+	public String[] getEntry(String columnName, String value) {
+		if(this.indices.containsKey(columnName.trim().toLowerCase())) {
+			Map<String, String[]> index = this.indices.get(columnName);
+			if(index.containsKey(value)) {
+				return index.get(value);
+			} else return null;
+		} else throw new IllegalArgumentException("Column name " + columnName + " is not an index column!");
 	}
 }
